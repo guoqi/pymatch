8cbd02f868e6a58fe6121f40551c84339a425cc1
qiguo(郭旗) <qiguo@tencent.com>
2018-12-03 20:43:38 +0800

    modify ast to create a syntax sugar

--- pymatch/syntax.py
+++ pymatch/syntax.py
@@ -43,13 +43,56 @@ class TransformWith(ast.NodeTransformer):
                 <statement_5>
     '''
     def visit_With(self, node):
+        '''
+        visit with-context statement
+        '''
+        if not isinstance(node.context_expr, ast.Call):
+            return node
+        if not isinstance(node.context_expr.func, ast.Name):
+            return node
+        if node.context_expr.func.id != "Match":
+            return node
         if node.optional_vars is None:
-            pass
+            node.optional_vars = self.do_with_match(node)
+        var_name = node.optional_vars.id
+        alter_body = []
+        pre_sub_with_stmts = None
+        for stmt in node.body[::-1]:
+            if not isinstance(stmt, ast.With):
+                alter_body.append(stmt)
+                continue
+            if pre_sub_with_stmts is None:
+                alter_stmt = self.do_with_case(var_name, stmt, stmt.body)
+            else:
+                alter_stmt = self.do_with_case(var_name, stmt, pre_sub_with_stmts)
+            pre_sub_with_stmts.append(alter_stmt)
+            alter_body.append(alter_stmt)
+        node.body = alter_body
+        return node
 
 
     def do_with_match(self, node):
+        return ast.Name(id="_sytax_sugar_tmp_var", ctx=ast.Store)
+
+
+    def add_with_finally_stmt(self, body):
         pass
 
 
-    def do_with_case(self, node):
-        pass
+    def do_with_case(self, ctx_id, node, orelse):
+        if not isinstance(node.context_expr, ast.Name):
+            return node
+        if not inspect.isclass(node.context_expr) and node.context_expr.id != "_":
+            return node
+        # convert to if-statement
+        if node.optional_vars is None:
+            return ast.If(
+                test = ast.Call(
+                    func=ast.Name(id="issubclass", ctx=ast.Load),
+                    args=[ast.Name(id=ctx_id, ctx=ast.Load), node.context_expr]
+                ),
+                body = node.body,
+                orelse = orelse
+            )
+        else:
+            pass
